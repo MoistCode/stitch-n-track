118e61330fb75dca60e6748968b31c34abc96b3a
moistcode <tpham@smugmug.com>
2023-02-23 20:36:47 -0800

    Main app changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectController } from './project/project.controller';
+import { ProjectService } from './project/project.service';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
+  controllers: [AppController, ProjectController, UserController],
+  providers: [AppService, ProjectService, PrismaService, UserService],
 })
 export class AppModule {}
